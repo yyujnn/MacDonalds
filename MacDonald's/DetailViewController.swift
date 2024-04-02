3db861b9666e547aa3a85085f2b2a84ba8229e90
yeggrrr <yjkim981103@gmail.com>
2024-04-02 16:50:47 +0900

    파일 복제해서 추가

---
+++ MacDonald's/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  MacDonald's
+//
+//  Created by 정유진 on 2024/04/02.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
