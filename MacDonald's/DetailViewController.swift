c5ef78a541e1af8bcbc183a52a51b0ff6adde69c
yeggrrr <yjkim981103@gmail.com>
2024-04-03 23:34:17 +0900

    코드 수정
    - Description 레이블 추가
    - 코드 정리
    - 데이터 저장소 사용하도록 수정

--- MacDonald's/DetailViewController.swift
+++ MacDonald's/DetailViewController.swift
@@ -10,53 +10,61 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var detailTableView: UITableView!
     @IBOutlet weak var menuImageView: UIImageView!
     @IBOutlet weak var totalAmountLabel: UILabel!
-    @IBOutlet weak var addToCart: UIButton!
-    @IBOutlet weak var goToCart: UIButton!
+    @IBOutlet weak var addToCartButton: UIButton!
+    @IBOutlet weak var goToCartButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
-    var cartList: [Menu] = [
-        MenuItem(name: "아보카도 치킨 샐러드",
-                 price: 5900,
-                 image: "avocadoChickenSalad",
-                 type: .salad)]
+    // MenuVC로부터 받아올 Menu 정보
+    var menu: Menu?
     
-    let menu: Menu = MenuItem(
-        name: "치즈 버거",
-        price: 3400,
-        image: "cheeze",
-        type: .burger)
-    
+    // 메인메뉴 주문 갯수
     var count: Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        menuImageView.image = .cheese
-
-        self.detailTableView.dataSource = self
-        self.detailTableView.delegate = self
+        
+        tableViewConfigure()
+        viewConfigure()
+    }
+    
+    // UI와 관련된 초기 설정
+    func viewConfigure() {
+        if let menu = menu {
+            menuImageView.image = UIImage(named: menu.image) ?? UIImage(systemName: "carrot.fill")!
+            descriptionLabel.text = menu.description
+        } else {
+            descriptionLabel.text = "잠시후 다시 시도해주세요."
+        }
+        
         self.totalAmountLabel.layer.masksToBounds = true
         self.totalAmountLabel.layer.cornerRadius = 15
-        self.addToCart.layer.masksToBounds = true
-        self.addToCart.layer.cornerRadius = 15
-        self.addToCart.backgroundColor = .systemPink
+        self.addToCartButton.layer.masksToBounds = true
+        self.addToCartButton.layer.cornerRadius = 15
+        self.addToCartButton.backgroundColor = .systemPink
         self.totalAmountLabel.backgroundColor = .lightGray
     }
+    
+    // 테이블뷰와 관련된 초기 설정
+    func tableViewConfigure() {
+        self.detailTableView.dataSource = self
+        self.detailTableView.delegate = self
+    }
+    
     // stepper 클릭시, 총 금액 업데이트
     @objc func stepperClicked(_ sender: UIStepper) {
-        print(">>> stepper clicked: \(sender.tag), sender.value: \(sender.value)")
         count = Int(sender.value)
         self.detailTableView.reloadData()
     }
     
     // 담기 버튼 alert 추가
-    @IBAction func addToCart(_ sender: Any) {
+    @IBAction func addToCartButtonClicked(_ sender: Any) {
         let alert = UIAlertController(title: "장바구니 담기", message: "해당 상품을 추가하시겠습니까?", preferredStyle: .alert)
+        
         // 추가 버튼
-        let addButton = UIAlertAction(title: "추가", style: .default, handler: { [ weak self ] _ in
-            let newItem = MenuItem(name: "치즈 버거", price: 3400, image: "cheeze", type: .burger)
-            self?.cartList.append(newItem)
-            self?.detailTableView.reloadData()
-            print("ClickedAddButton")
+        let addButton = UIAlertAction(title: "추가", style: .default, handler: { _ in
+            guard let menu = self.menu else { return }
+            DataStorage.cartList.append(menu)
+            self.detailTableView.reloadData()
         })
         // 취소 버튼
         let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
@@ -65,23 +73,21 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         self.present(alert, animated: true, completion: nil)
     }
     
-    
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath) as? ListCell else { return UITableViewCell() }
+        guard let menu = menu else { return UITableViewCell() }
         cell.menuNameLabel.text = menu.name
         cell.menuPriceLabel.text = "₩\(menu.price)"
-        let totalAmount = (menu.price) * count
-        
         cell.countLabel.text = "\(count)"
+        let totalAmount = (menu.price) * count
+        totalAmountLabel.text = "총 금액: \(totalAmount)원"
+        
         cell.countStepper.tag = indexPath.row
         cell.countStepper.addTarget(self, action: #selector(stepperClicked), for: .valueChanged)
-        totalAmountLabel.text = "총 금액: \(totalAmount)원"
         return cell
     }
 }
@@ -91,20 +97,4 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var menuPriceLabel: UILabel!
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var countStepper: UIStepper!
-
-// - stepper 카운트
-//    @IBAction func presentStepper(_ sender: UIStepper) {
-//        countLabel.text = Int(sender.value).description
-//    }
-    
-// - 뒤로가기 버튼이 필요하다면!
-// @IBAction func closeButton(_ sender: Any) {
-//          dissmiss(animated: true, completion: nil)
-// }
-    
-// - 데이터 담아서 저장하는 코드 만들기
-    
-    
 }
-
-
