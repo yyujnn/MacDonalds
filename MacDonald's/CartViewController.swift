980bbb78577e3a642f01215a204f756525c87c58
정유진 <jeong-yujin@jeong-yujin-ui-MacBookAir.local>
2024-04-03 16:31:50 +0900

    updateTotal

--- MacDonald's/CartViewController.swift
+++ MacDonald's/CartViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-// 가상 데이터 모델
+// MARK: - 가상 데이터 모델
 struct Menu2 {
     var name: String
     var description: String?
@@ -46,10 +46,12 @@ struct OrderMenu {
     var quantity: Int
 }
 
-
+// MARK: - 장바구니 뷰컨트롤러
 class CartViewController: UIViewController {
     
     @IBOutlet weak var orderListTableView: UITableView!
+    @IBOutlet weak var orderTotalCount: UILabel!
+    @IBOutlet weak var orderTotalPrice: UILabel!
     @IBOutlet weak var paymentButton: UIButton!
     
     // 임시 주문 배열 넘어왔다고 가정
@@ -68,11 +70,29 @@ class CartViewController: UIViewController {
         let cartTableViewCellNib = UINib(nibName: String(describing: CartTableViewCell.self), bundle: nil)
         
         self.orderListTableView.register(cartTableViewCellNib, forCellReuseIdentifier: "CartTableViewCell")
-        
         self.paymentButton.layer.cornerRadius = 20
         
+        // 총 가격 업데이트
+        self.updateTotal()
     }
+    
+    // MARK: - 총 가격 설정 메서드
+    func updateTotal() {
+        var totalCount = 0
+        var totalPrice = 0
+        
+        for order in orderArray {
+            totalCount += order.quantity
+            totalPrice += (order.menu.price) * (order.quantity)
+        }
+        
+        orderTotalCount.text = "\(totalCount)"
+        orderTotalPrice.text = "₩\(totalPrice)"
+    }
+
 }
+
+// MARK: - DataSource
 extension CartViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.orderArray.count
@@ -97,9 +117,12 @@ extension CartViewController: UITableViewDataSource {
             // 가격 업데이트
             let newPrice = order.menu.price * quantity
             
+            // cell의 수량 및 가격 설정하기
             cell.updatePriceLabel(newPrice: newPrice, quantity: quantity)
-            
+
             print("스텝퍼 수정: \(orderArray)")
+            
+            self.updateTotal()
         }
         
         return cell
