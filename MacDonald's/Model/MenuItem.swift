d503f3e1109ea25b8df9fe8dd1b96392f1bbbab9
mgynsz <mgynsz@gmail.com>
2024-04-03 12:50:11 +0900

    update model data

--- MacDonald's/Model/MenuItem.swift
+++ MacDonald's/Model/MenuItem.swift
@@ -8,53 +8,42 @@
 import Foundation
 
 
-// 콤보 메뉴
-let tripleBurgerCombo = Combo(name: "트리플 버거 콤보", description: "Three quarters of a pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. Use both hands!", price: "6,900원", image: "tripleCombo", burger: tripleBurger.name, sideMenu: friesSmall.name, drink: coke.name)
-
-let doubleBaconBurgerCombo = Combo(name: "더블 베이컨 콤보", description: "A half-pound* of fresh British beef, American cheese, 6 pieces of crispy Applewood smoked bacon, Heinz ketchup, and Heinz mayo. Carnivores rejoice!", price: "8,200원", image: "doubleBaconCombo", burger: doubleBaconBurger.name, sideMenu: friesMedium.name, drink: coke.name)
-
-let singleBurgerCombo = Combo(name: "싱글 버거 콤보", description: "A quarter-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun just the way Dave intended.", price: "5,500원", image: "singleCombo", burger: singleBurger.name, sideMenu: friesMedium.name, drink: coke.name)
-
-let doubleBurgerCombo = Combo(name: "더블 버거 콤보", description: "A half-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. More meat means more to love.  May contain EGG.", price: "6,200원", image: "doubleCombo", burger: doubleBurger.name, sideMenu: friesMedium.name, drink: coke.name)
-
-
-
 // 햄버거 메뉴
-let baconCheeseBurger = Burger(name: "베이컨 치즈 버거", description: "Fresh British beef, Applewood smoked bacon, American cheese, crisp lettuce, tomato, and Heinz mayo. Its a favourite of bacon lovers everywhere.", calories: "406 kcal", price: "3,700원", image: "baconCheese")
+let baconCheese = MenuItem(name: "베이컨 치즈 버거", description: "Fresh British beef, Applewood smoked bacon, American cheese, crisp lettuce, tomato, and Heinz mayo. Its a favourite of bacon lovers everywhere.", price: 3700, image: "baconCheese", calories: 406, type: .burger)
 
-let cheeseBurger = Burger(name: "치즈 버거", description: "Fresh British beef topped with cheese, pickles, onions, tomatoes, crisp lettuce, Heinz ketchup, and Heinz mayo. It's big flavour at a savoury price.", calories: "375 kcal", price: "2,800원", image: "cheese")
+let cheese = MenuItem(name: "치즈 버거", description: "Fresh British beef topped with cheese, pickles, onions, tomatoes, crisp lettuce, Heinz ketchup, and Heinz mayo. It's big flavour at a savoury price.", price: 2800, image: "cheese", calories: 375, type: .burger)
 
-let doubleBaconBurger = Burger(name: "더블 베이컨 버거", description: "A half-pound* of fresh British beef, American cheese, 6 pieces of crispy Applewood smoked bacon, Heinz ketchup, and Heinz mayo. Carnivores rejoice!", calories: "1010 kcal", price: "6,200원", image: "doubleBacon")
+let doubleBacon = MenuItem(name: "더블 베이컨 버거", description: "A half-pound* of fresh British beef, American cheese, 6 pieces of crispy Applewood smoked bacon, Heinz ketchup, and Heinz mayo. Carnivores rejoice!", price: 6200, image: "doubleBacon", calories: 1010, type: .burger)
 
-let tripleBurger = Burger(name: "트리플 버거", description: "Three quarters of a pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. Use both hands!", calories: "1204 kcal", price: "5,400원", image: "triple", cheeseOption: .none, onionOption: .none, pattyOption: .none)
+let triple = MenuItem(name: "트리플 버거", description: "Three quarters of a pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. Use both hands!", price: 5400, image: "triple", calories: 1204, type: .burger)
 
-let doubleBurger = Burger(name: "더블 버거", description: "A half-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. More meat means more to love.", calories: "888 kcal", price: "4,800원", image: "double")
+let double = MenuItem(name: "더블 버거", description: "A half-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun. More meat means more to love.", price: 4800, image: "double", calories: 888, type: .burger)
 
-let singleBurger = Burger(name: "싱글 버거", description: "A quarter-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun just the way Dave intended.", calories: "524 kcal", price: "3,900원", image: "single")
+let single = MenuItem(name: "싱글 버거", description: "A quarter-pound* of fresh British beef, American cheese, crisp lettuce, tomato, pickle, Heinz ketchup, Heinz mayo, and onion on a toasted bun just the way Dave intended.", price: 3900, image: "single", calories: 524, type: .burger)
 
 
 // 사이드 메뉴
-let cheesepotato = SideMenu(name: "치즈 감자 베이크", description: "Hot and fluffy potato topped with creamy cheese sauce, and shredded cheese. Because nothing goes with a potato like cheese.", calories: "342 kcal", price: "1,100원", image: "cheesepotato")
+let cheesepotato = MenuItem(name: "치즈 감자 베이크", description: "Hot and fluffy potato topped with creamy cheese sauce, and shredded cheese. Because nothing goes with a potato like cheese.", price: 1100, image: "cheesepotato", calories: 342, type: .side)
 
-let chiliCheesePotato = SideMenu(name: "칠리 치즈 감자 베이크", description: "Hot and fluffy potato topped with Wendy's signature meaty, flavourful chili, creamy cheese sauce and shredded cheese. This will be your best bud.", calories: "428 kcal", price: "1,400원", image: "chiliCheesePotato")
+let chiliCheesePotato = MenuItem(name: "칠리 치즈 감자 베이크", description: "Hot and fluffy potato topped with Wendy's signature meaty, flavourful chili, creamy cheese sauce and shredded cheese. This will be your best bud.", price: 1400, image: "chiliCheesePotato", calories: 428, type: .side)
 
-let chiliCheeseFries = SideMenu(name: "칠리 치즈 감자튀김", description: "Our natural-cut, skin-on, sea-salted fries topped our hearty chili and warm, creamy cheese sauce,  and shredded cheddar cheese. Easy to love. Hard to beat.", calories: "481 kcal", price: "2,800", image: "chiliCheeseFries")
+let chiliCheeseFries = MenuItem(name: "칠리 치즈 감자튀김", description: "Our natural-cut, skin-on, sea-salted fries topped our hearty chili and warm, creamy cheese sauce,  and shredded cheddar cheese. Easy to love. Hard to beat.", price: 2800, image: "chiliCheeseFries", calories: 481, type: .side)
 
-let friesSmall = SideMenu(name: "감자튀김 스몰", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", calories: "176 kcal", price: "900원", image: "friesSmall")
+let friesSmall = MenuItem(name: "감자튀김 스몰", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", price: 900, image: "friesSmall", calories: 176, type: .side)
 
-let friesMedium = SideMenu(name: "감자튀김 미디움", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", calories: "239 kcal", price: "1,200원", image: "friesMedium")
+let friesMedium = MenuItem(name: "감자튀김 미디움", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", price: 1200, image: "friesMedium", calories: 239, type: .side)
 
-let friesLarge = SideMenu(name: "감자튀김 라지", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", calories: "299 kcal", price: "1,400원", image: "friesLarge")
+let friesLarge = MenuItem(name: "감자튀김 라지", description: "Natural-cut, skin-on, sea-salted fries served hot and crispy. The world loves them for a reason.", price: 1400, image: "friesLarge", calories: 299, type: .side)
 
 
 // 음료 메뉴
-let coke = Drink(name: "콜라", description: "시원한 음료", price: "1,200원", image: "coke")
-let cokeZero = Drink(name: "제로 콜라", description: "시원한 음료", price: "1,200원", image: "cokeZero")
-let sprite = Drink(name: "스프라이트", description: "시원한 음료", price: "1,200원", image: "sprite")
+let coke = MenuItem(name: "콜라", price: 1200, image: "coke", type: .drink)
+let cokeZero = MenuItem(name: "제로 콜라", price: 1200, image: "cokeZero", type: .drink)
+let sprite = MenuItem(name: "스프라이트", price: 1200, image: "sprite", type: .drink)
 
 
 // 샐러드 매뉴
-let avocadoChickenSalad = SaladMenu(name: "아보카도 치킨 샐러드", description: "Made fresh daily with our signature lettuce blend, cheddar cheese, diced tomatoes, cool, creamy avocado, Applewood smoked bacon, and grilled chicken breast hot off the grill, all topped with a Southwest Ranch Dressing. ", calories: "485 kcal", price: "5,900원", image: "avocadoChickenSalad")
+let avocadoChickenSalad = MenuItem(name: "아보카도 치킨 샐러드", description: "Made fresh daily with our signature lettuce blend, cheddar cheese, diced tomatoes, cool, creamy avocado, Applewood smoked bacon, and grilled chicken breast hot off the grill, all topped with a Southwest Ranch Dressing. ", price: 5900, image: "avocadoChickenSalad", calories: 485, type: .salad)
 
-let caesarChikenSalad = SaladMenu(name: "시저 치킨 샐러드", description: "Made fresh daily with romaine lettuce, grilled chicken breast, Italian cheese, and creamy Caesar dressing. One bite will tell you why its king of more than just the Romaines.", calories: "481 kcal", price: "4,600원", image: "caesarChikenSalad")
+let caesarChikenSalad = MenuItem(name: "시저 치킨 샐러드", description: "Made fresh daily with romaine lettuce, grilled chicken breast, Italian cheese, and creamy Caesar dressing. One bite will tell you why its king of more than just the Romaines.", price: 4600, image: "caesarChikenSalad", calories: 481, type: .salad)
 
